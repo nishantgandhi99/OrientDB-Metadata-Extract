0b671c64c80e55efae7219786c92065c942bf362
mendon26 <mendon.a@husky.neu.edu>
2018-04-04 14:53:00 -0400

    All statements are printed on screen. Need to add everything to a text file. Also need to make it configurable.

--- src/main/java/AppRunner.java
+++ src/main/java/AppRunner.java
@@ -1,10 +1,15 @@
+
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 
-class AppRunner {
+public class AppRunner {
 
     public static void main(String[] args) {
 
@@ -14,18 +19,32 @@ class AppRunner {
             Properties info = new Properties();
             info.put("user", "admin");
             info.put("password", "admin");
-            Connection conn = DriverManager.getConnection("jdbc:orient:plocal:OpenBeer", info);
+            Connection conn = DriverManager.getConnection("jdbc:orient:remote:localhost/OpenBeer", info);//TO DO: OpenBeer will be configurable
             Statement stmt = conn.createStatement();
 
-            ResultSet rs = stmt.executeQuery("select expand(classes) from metadata:schema");
+            ResultSet rs = stmt.executeQuery("SELECT from (select expand(classes) from metadata:schema) "
+                    + "where \"V\" in superClass;");
+            print(rs);
+            rs.close();
+            rs = stmt.executeQuery("SELECT name from (select expand(classes) from metadata:schema) "
+                    + "where \"V\" in superClass;");
+            ArrayList <String> tableList = new ArrayList<String>();
+          while (!rs.isLast()) {
+            rs.next();
+            
+                String string = rs.getString("name");
+                tableList.add(string);
+          }
+           rs.close();
+          for(String s : tableList){
+              System.out.println(s);
+              rs = stmt.executeQuery("select expand(properties) from (select expand(classes) from metadata:schema) "
+                      + "where name = '"+s+"'");
+            print(rs);
+          }
 
-            while (!rs.isLast()) {
-                rs.next();
-                System.out.println(rs.getString("name"));
-            }
             rs.close();
             stmt.close();
-
 
         } catch (Exception e) {
             System.out.println(e);
@@ -33,5 +52,27 @@ class AppRunner {
 
     }
 
+    public static void print(ResultSet rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        int colCount = md.getColumnCount();
+
+        for (int i = 1; i <= colCount; i++) {
+
+            String col_name = md.getColumnName(i);
+
+            System.out.print(col_name + " || ");
+        }
+        System.out.println();
+        while (rs.next()) {
+            
+            for (int i = 1; i <= colCount; i++) {
+
+                String col_name = md.getColumnName(i);
+
+                System.out.print(rs.getNString(col_name) + " || ");
+            }
+            System.out.println();
+        }
+    }
 
 }
